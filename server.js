cc46cd851cfc075a7b1543c623af9da135033f3a
AndreyKorolevich <korolevich1994mail.ru>
2020-05-05 21:21:14 +0300

    Added get. and delete method

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const googleSheets = require('gsa-sheets');
-
 const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = '1boTzRAnIv7z25iHA0U7xeGGhR87r25NCHRdhKocWN5k';
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -17,11 +16,16 @@ app.use(express.static('public'));
 async function onGet(req, res) {
   const result = await sheet.getRows();
   const rows = result.rows;
-  console.log(rows);
+  let arrResult = [];
 
-  // TODO(you): Finish onGet.
-
-  res.json( { status: 'unimplemented'} );
+  for(let i = 1; i < rows.length; i++){
+    const user = {};
+    for(let j = 0; j < rows[0].length; j++){
+      user[rows[0][j]] = rows[i][j]
+    }
+    arrResult.push(user);
+  }
+  res.json(arrResult);
 }
 app.get('/api', onGet);
 
@@ -46,12 +50,18 @@ async function onPatch(req, res) {
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
 async function onDelete(req, res) {
-  const column  = req.params.column;
+  const result = await sheet.getRows();
+  const rows = result.rows;
   const value  = req.params.value;
 
-  // TODO(you): Implement onDelete.
-
-  res.json( { status: 'unimplemented'} );
+  for(let i = 1; i < rows.length; i++){
+   if(rows[i].indexOf(value) !== -1) {
+    const elem = await sheet.deleteRow(i);
+    res.json(elem);
+    return
+   }
+  }
+  res.json({"response": "success"});
 }
 app.delete('/api/:column/:value',  onDelete);
 
@@ -60,5 +70,5 @@ app.delete('/api/:column/:value',  onDelete);
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log(`Server listening on port ${port}!`);
+  console.log(`CS193X: Server listening on port ${port}!`);
 });
